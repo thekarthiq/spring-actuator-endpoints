629cbb1ebba938aff9d3993ceda29ca30d61d915
Karthik Vignesh <thekarthiq@gmail.com>
2022-05-11 00:06:03 +0530

    first commit

---
+++ src/test/java/com/develper/vignesh/springactuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develper.vignesh.springactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
